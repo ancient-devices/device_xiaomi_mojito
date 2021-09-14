f3a09ffb028600a0a64718c4c5bf3b376b3a0a23
Alexander Koskovich <zvnexus@outlook.com>
2021-09-14 23:17:27 +0600

    mojito: Remove virtual framebuffer kernel cmdline
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I51becacd30d17e71cdeb0fbe31bbe1b94262b316

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,7 +134,6 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
 TARGET_KERNEL_NEW_GCC_COMPILE := true
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc64/bin/aarch64-elf-
